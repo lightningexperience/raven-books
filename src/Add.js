b45338b5ec06d79ee51eff644b368b65c2fc993f
lightningexperience <lightningexperience@users.noreply.github.com>
2021-03-17 13:17:13 -0400

    Update Add.js

--- src/Add.js
+++ src/Add.js
@@ -23,7 +23,7 @@ export default function Add(props) {
 
   return (
     <form className="add-review" onSubmit={handleSubmit(onSubmit)}>
-      <h4>Add Review</h4>
+      <h4>Add Lead</h4>
       <input
         type="text"
         placeholder="Book Title"
